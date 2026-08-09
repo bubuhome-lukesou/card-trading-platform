06d7f75b6a6ac49b7b7a62974e2c63bbb00f9c5f
Bubu <bubu@bubu.com>
2026-08-09 20:04:06 +0800

    fix: remove transition out-in causing blank page on browser back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,21 +6,9 @@ import AppLayout from '@/components/layout/AppLayout.vue'
 <template>
   <AppLayout>
     <RouterView v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
+      <component :is="Component" />
     </RouterView>
   </AppLayout>
 </template>
 
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
